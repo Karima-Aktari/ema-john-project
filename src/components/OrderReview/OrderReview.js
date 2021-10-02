e4503ad274d83325a0600b93696c5c1e7d3e0a5e
Karima-Aktari <karimaaktari911@gmail.com>
2021-10-02 17:38:23 +0600

    commit again

--- src/components/OrderReview/OrderReview.js
+++ src/components/OrderReview/OrderReview.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import useCart from '../../hooks/useCart';
+import useProducts from '../../hooks/useProducts';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
+import { clearTheCart, deleteFromDb } from '../../utilities/fakedb'
+import { useHistory } from 'react-router-dom';
 
 const OrderReview = () => {
+    const [products, setProducts] = useProducts();
+    const [cart, setCart] = useCart(products);
+    const history = useHistory();
+
+    const handleRemove = key => {
+        const newCart = cart.filter(product => product.key !== key);
+        setCart(newCart);
+        // fakedb theke export kora hoise
+        deleteFromDb(key);
+    }
+
+    const handlePlaceOrder = () => {
+        history.push('/placeorder');
+        setCart([]);
+        clearTheCart();
+    }
+
     return (
-        <div>
-            <h2>This is Order Review</h2>
+        <div className="shop-container">
+            <div className="product-container">
+                {
+                    cart.map(product => <ReviewItem
+                        key={product.key}
+                        product={product}
+                        handleRemove={handleRemove}
+                    ></ReviewItem>)
+                }
+            </div>
+
+            <div className="cart-container">
+                <Cart cart={cart}>
+                    <button onClick={handlePlaceOrder} className="btn-regular">Place order</button>
+                </Cart>
+            </div>
         </div>
     );
 };
